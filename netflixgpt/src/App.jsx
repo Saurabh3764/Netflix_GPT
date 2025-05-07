0d5af6da2d17d5d1f0e1e5aa0244e9d308cca30c
Saurabh Choudhary <saurabh.choudhary@emerson.com>
2025-05-07 18:49:13 +0530

    Setup done with Vite, Tailwind and React Router DOM Libraries.

---
+++ netflixgpt/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Browse from './Components/Browse'
+import Header from './Components/Header'
+ 
+function App() {
+  
+
+  return (
+    <>
+       <Header/>
+       <Browse/>
+    </>
+  )
+}
+
+export default App
---
+++ netflixgpt/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex flex-col'>
+        Header
+    </div>
+  )
+}
+
+export default Header
